1910613d63dd07f1bf2ca59e852c3edd216521d5
BondAlexander <alexancb@rams.colostate.edu>
2021-06-28 20:46:37 -0600

    GOT THE SERVER WORKING

--- Server/StorageServer.py
+++ Server/StorageServer.py
@@ -7,26 +7,33 @@ import shutil
 import time
 import os
 from threading import Thread
+import ffmpeg
 
+
+# RESOLUTION = (426, 240)
 RESOLUTION = (1280, 720)
 FPS = 20
 
-PORT = 8002
+PORT = 8001
 LEGAL_PORTS = [8005, 8006, 8007, 8008]
 FORMAT = 'utf-8'
-FOURCC = VideoWriter_fourcc(*'DIVX')
+FOURCC = VideoWriter_fourcc(*'mp4v')
 SUCCESS_MSG = bytes(f'{"SUCCESS":<10}', 'utf-8')
 FAILURE_MSG = bytes(f'{"FAILURE":<10}', 'utf-8')
 
 
 def merge_clip(ip_addr):
     date_formatted = "6.24.2021"
-    output_file_path = f'Footage/{ip_addr}/{date_formatted}.mp4'
-    tmp_clip_file_path = f'.tmp/{ip_addr}/clip.mp4'
+    version = 0
+    output_file_path = f'Footage/{ip_addr}/{date_formatted}_v{version}.mkv'
+    tmp_clip_file_path = f'.tmp/{ip_addr}/clip.mp4.tmp'
     if os.path.exists(output_file_path):
-        os.system(f'mencoder {output_file_path} {tmp_clip_file_path} -ovc copy -oac copy -of lavf format=mp4 -o {output_file_path}')
+        os.system(f'mkvmerge -o {output_file_path}.tmp {output_file_path} \+ {tmp_clip_file_path}')
+        os.remove(output_file_path)
+        shutil.copy(f'{output_file_path}.tmp', output_file_path)
+        os.remove(tmp_clip_file_path)
     else:
-        shutil.copyfile(tmp_clip_file_path, output_file_path)
+        shutil.copy(tmp_clip_file_path, output_file_path)
 
 def start_instance(conn, addr):
     print(f'\t{addr[0]} Connected')
@@ -45,72 +52,73 @@ def start_instance(conn, addr):
     all_frames = []
     new_frame = True
     recv_time_array = []
+    try:
+        # Poll for frames
+        while True:
+            
 
-    # Poll for frames
-    while True:
-        recv_time = time.time()
-
-        if new_frame:
-            frame_num = 0
-            frame_size = 0
-            message = conn.recv(30)
-            try:
-                frame_size = int(message[4:15])
-                if frame_size == 0:
+            if new_frame:
+                recv_time = time.time()
+                message = conn.recv(30)
+                try:
+                    frame_size = int(message[4:15])
+                    if frame_size == 0:
+                        conn.send(FAILURE_MSG)
+                        continue
+                except ValueError as e:
                     conn.send(FAILURE_MSG)
                     continue
-            except ValueError as e:
-                conn.send(FAILURE_MSG)
+                try:
+                    frame_num = int(message[18:])
+                except ValueError as e:
+                    conn.send(FAILURE_MSG)
+                else:
+                    new_frame = False
+                    conn.send(SUCCESS_MSG)
                 continue
-            try:
-                frame_num = int(message[18:])
-            except ValueError as e:
-                conn.send(FAILURE_MSG)
             else:
-                new_frame = False
-                conn.send(SUCCESS_MSG)
-            
-            continue
-        else:
-            try:
+
                 message = conn.recv(frame_size)
-            except ValueError:
-                print('value error')
-                conn.send(FAILURE_MSG)
-            else:
                 frame += message
             
-        if len(frame) > frame_size :
-            conn.send(FAILURE_MSG)
-            frame = b''
-        elif len(frame) == frame_size:
-            recv_time_array.append(time.time() - recv_time)
-
-            # TODO REMOVE THIS TRY BLOCK AFTER HASHING
-            try:
-                image = imdecode(np.asarray(bytearray(frame), dtype="uint8"), IMREAD_COLOR)
-            except Exception as e:
+            if len(frame) > frame_size :
                 conn.send(FAILURE_MSG)
-                continue
-            else: 
-                all_frames.append({'frame_num': frame_num, 'frame':image})
-                # TODO make if statement to see if len(all_frames) / FPS == 7
-                # If true then spin up process to append clip to main footage for day
-                # Then send success msg
-                if len(all_frames) / FPS == 1:
-                    def sort_frames(i):
-                        return int(i['frame_num'])
-                    all_frames.sort(key=sort_frames)
-                    for frame in all_frames:
-                        video.write(frame['frame'])
-                    video.release()
-                    merge_clip(addr[0])
-                    all_frames = []
-                conn.send(SUCCESS_MSG)
-
                 frame = b''
-                new_frame = True
-                
+            elif len(frame) == frame_size:
+
+                # TODO REMOVE THIS TRY BLOCK AFTER HASHING
+                try:
+                    image = imdecode(np.asarray(bytearray(frame), dtype="uint8"), IMREAD_COLOR)
+                except Exception as e:
+                    conn.send(FAILURE_MSG)
+                    continue
+                else: 
+                    all_frames.append({'frame_num': frame_num, 'frame':image})
+
+                    if len(all_frames) / FPS == 10:
+                        def sort_frames(i):
+                            return int(i['frame_num'])
+                        all_frames.sort(key=sort_frames)
+                        for frame in all_frames:
+                            video.write(frame['frame'])
+                        video.release()
+                        shutil.copy(f'{tmp_clip_file_path}', f'{tmp_clip_file_path}.tmp')
+                        # merge_clip(addr[0])
+                        Thread(target=merge_clip, args=(addr[0],))
+                        all_frames = []
+                        video = VideoWriter(tmp_clip_file_path, FOURCC, FPS, RESOLUTION)
+                    conn.send(SUCCESS_MSG)
+
+                    frame = b''
+                    new_frame = True
+                    recv_time_array.append(time.time() - recv_time)
+    finally:
+        sum_time = 0.0
+        for t in recv_time_array:
+            sum_time += t
+        print(f'Average time per frame: {sum_time/len(recv_time_array)}')
+
+            
 
 def main():
     
